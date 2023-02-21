d8368916ed4407f88c3f18363d6fc06ea840086f
skorodenko <mskrodenko@gmail.com>
2023-02-21 19:02:55 +0200

    Asyncio reduce asyncio sleep

--- soloviy/widgets/init_main.py
+++ soloviy/widgets/init_main.py
@@ -75,7 +75,7 @@ class InitMainWindow(QMainWindow, Ui_MainWindow):
             self.media_seek.setMaximum(duration)
             self.media_seek.setSliderPosition(elapsed)
 
-            await asyncio.sleep(0.35)
+            await asyncio.sleep(0.4)
 
     async def _change_playlist(self, index):
         await self.ptiling_widget.add_playlist(index.data())
